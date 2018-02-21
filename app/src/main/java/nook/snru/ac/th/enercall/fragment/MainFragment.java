6d761f9a4c938fb41c9316c2db8f14c1b6e8d43a
nook <Thayalak.nk59@snru.ac.th>
2018-02-21 14:17:02 +0700

    Last

--- app/src/main/java/nook/snru/ac/th/enercall/fragment/MainFragment.java
+++ app/src/main/java/nook/snru/ac/th/enercall/fragment/MainFragment.java
@@ -3,9 +3,12 @@ package nook.snru.ac.th.enercall.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import nook.snru.ac.th.enercall.R;
 
@@ -14,6 +17,49 @@ import nook.snru.ac.th.enercall.R;
  */
 
 public class MainFragment extends Fragment{
+
+    // Explicit
+    private String tag = "MyTagV1";
+
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // For Station l
+        ImageView station1ImageView = getView().findViewById(R.id.imageV1ewStation1);
+        station1ImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d(tag,"You click Image station1");
+
+            }
+        });
+
+        TextView station1TextView =getView().findViewById(R.id.textViewStation1);
+        station1TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(tag, "Click Text" +getString(R.string.station1));
+            }
+        });
+
+
+        // For Station 2
+
+        // For Station 3
+
+        // For Station 4
+
+
+
+    } //Main Method
+
+    public void callStation(String numberCall) {
+
+    }
 
     @Nullable
     @Override
