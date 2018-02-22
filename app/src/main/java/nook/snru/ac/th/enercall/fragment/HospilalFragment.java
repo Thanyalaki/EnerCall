a75d73dac4e634ad6ec4b523bc5a749ff71bb5a2
nook <Thayalak.nk59@snru.ac.th>
2018-02-22 11:58:53 +0700

    before Lunch

---
+++ app/src/main/java/nook/snru/ac/th/enercall/fragment/HospilalFragment.java
@@ -0,0 +1,31 @@
+package nook.snru.ac.th.enercall.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nook.snru.ac.th.enercall.R;
+
+/**
+ * Created by Admin on 22/2/2561.
+ */
+
+public class HospilalFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_hospital, container, false);
+        return view;
+    }
+}  // Main Class
+
